fff4d6d4e89285d8606fc3597371888391ad2432
Juan Reisz <juan.reisz@techsquads.com>
2023-12-30 20:03:30 -0300

    added base url on vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({
-    compilerOptions: {
-      dev: true
-    }
-  })],
-})
+  base: "/xdviewer/",
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev: true,
+      },
+    }),
+  ],
+});
